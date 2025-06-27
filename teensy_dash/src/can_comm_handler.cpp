f97595fd65759bf7e9f149daa46f05304ed2c6d0
Yves Bonneau <yves.bonneau2004@gmail.com>
2025-06-27 02:56:01 +0100

    fixed front wheel rpm calculations; rm torque send to bamocar

--- teensy_dash/src/can_comm_handler.cpp
+++ teensy_dash/src/can_comm_handler.cpp
@@ -26,6 +26,7 @@ void CanCommHandler::setup() {
   can1.setFIFOFilter(1, BAMO_RESPONSE_ID, STD);
   can1.setFIFOFilter(2, MASTER_ID, STD);
   can1.onReceive(can_snifflas);
+  DEBUG_PRINTLN("WAITING 10 SEC");
   delay(10000);
 
   CAN_message_t disable;
@@ -135,7 +136,7 @@ void CanCommHandler::bamocar_callback(const uint8_t* const msg_data, const uint8
 
     case SPEED_ACTUAL:
       updatable_data.speed = message_value;
-      DEBUG_PRINTLN("BAMOCAR SPEED: " + String(message_value));
+      // DEBUG_PRINTLN("BAMOCAR SPEED: " + String(message_value));
       break;
 
     // Next cases are for debug only.
@@ -209,6 +210,7 @@ void CanCommHandler::master_callback(const uint8_t* const msg_data, const uint8_
 void CanCommHandler::write_messages() {
   if (rpm_timer >= RPM_MSG_PERIOD_MS) {
     write_rpm();
+    write_bamocar_speed();
     rpm_timer = 0;
   }
 
